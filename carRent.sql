17c8e2d8cb9270bc73645d207fc7e72431aee7e8
Arnadillo <arnadillo@ubuntu.(none)>
2014-11-25 01:09:27 -0700

    Updated Tables

--- carRent.sql
+++ carRent.sql
@@ -3,7 +3,8 @@
  * Submitted by Spencer Howe, Tristan Schorn, Arnold Padillo
  * 
  * NOTES:
- *	- Can test with http://sqlfiddle.com/ 
+ *	- Can test with http://sqlfiddle.com/
+ *	- Made SQL Statements in Upper case (for recognition)
  *
  * TODO:
  * - Add triggers
@@ -13,50 +14,66 @@
  */
 
 CREATE TABLE Vehicle(
-	SERIAL_NO INT PRIMARY KEY NOT NULL,
-	CURR_ODOMETER INT NOT NULL,
-	FUEL_TYPE VARCHAR (20) NOT NULL,
-	PLATE_NO VARCHAR (20) NOT NULL,
-	COLOUR VARCHAR (20) NOT NULL,
-	MODEL VARCHAR (20) NOT NULL,
-	BASE_COST DECIMAL(10, 2) NOT NULL, -- Max of 10 Digits, 2 numbers after decimal
-	MAKE VARCHAR(20) NOT NULL,
-	NO_SEATS INT NOT NULL,
-	TYPE VARCHAR(20)
+	serial_no INT PRIMARY KEY NOT NULL,
+	current_Odometer INT NOT NULL,
+	fuel_type VARCHAR (20) NOT NULL,
+	plate_no VARCHAR (20) NOT NULL,
+	colour VARCHAR (20) NOT NULL,
+	model VARCHAR (20) NOT NULL,
+	base_cost DECIMAL(10, 2) NOT NULL, -- Max of 10 Digits, 2 numbers after decimal
+	make VARCHAR(20) NOT NULL,
+	no_seats INT NOT NULL,
+	type VARCHAR(20)
 );
 
 CREATE TABLE Branch(
-	BRANCH_NO INT PRIMARY KEY NOT NULL,
-	LOCATION VARCHAR(30) NOT NULL,
-	ADDRESS VARCHAR(30) NOT NULL,
-	BRANCH_NAME VARCHAR(30) NOT NULL
+	branch_no INT PRIMARY KEY NOT NULL,
+	location VARCHAR(30) NOT NULL,
+	address VARCHAR(30) NOT NULL,
+	branch_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE Customer(
-	LICENSE_NO INT PRIMARY KEY NOT NULL,
-	CUST_NAME VARCHAR (30) NOT NULL,
-	PHONE_NO INT NOT NULL,
-	CREDCARD_NO INT NOT NULL
+	license_no INT PRIMARY KEY NOT NULL,
+	name VARCHAR (30) NOT NULL,
+	phone_no INT NOT NULL,
+	creditCard_no INT NOT NULL
 );
 
 CREATE TABLE Employee(
-	EMPLOYEE_ID INT PRIMARY KEY NOT NULL,
-	SUPERVISOR_ID INT NOT NULL,
-	EMP_NAME VARCHAR (30) NOT NULL
+	employee_id INT PRIMARY KEY NOT NULL,
+	supervisor_id INT NOT NULL,
+	name VARCHAR (30) NOT NULL
 );
 
 CREATE TABLE WorksAt(
-	BRANCH_NO INT NOT NULL,
-	POSITION VARCHAR (30) NOT NULL,
-	EMP_ID INT NOT NULL
+	branch_no INT NOT NULL,
+	position VARCHAR (30) NOT NULL,
+	employee_id INT NOT NULL
 );
 
 CREATE TABLE Rental(
-	RENTAL_NO INT PRIMARY KEY NOT NULL,
-	CUST_LICEN INT NOT NULL,
-	BRANCH_NO INT NOT NULL,
-	EMP_NAME VARCHAR (30) NOT NULL,
-	VEH_NO VARCHAR (30) NOT NULL
+	rental_no INT PRIMARY KEY NOT NULL,
+	license_no INT NOT NULL,
+	branch_no INT NOT NULL,
+	employee_id VARCHAR (30) NOT NULL,
+	serial_no VARCHAR (30) NOT NULL,
+	date_range VARCHAR (20) NOT NULL,
+	insurance_package VARCHAR (30) NOT NULL,
+	rental_cost DECIMAL(10, 2) NOT NULL
+);
+
+CREATE TABLE Credentials(
+	username VARCHAR (30) PRIMARY KEY NOT NULL,
+	hashed_pass VARCHAR (30) NOT NULL
+);
+
+CREATE TABLE Inventory(
+	branch_no INT NOT NULL,
+	no_vehicle_rented INT,
+	serial_no INT PRIMARY KEY NOT NULL,
+	is_rented BOOLEAN DEFAULT FALSE
+
 );
 
 
